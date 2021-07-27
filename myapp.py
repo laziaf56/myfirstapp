39887b944106ca74b51fc14e2ff4efce5bd2c830
laziaf56 <87532054+laziaf56@users.noreply.github.com>
2021-07-27 12:09:24 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,17 +1,4 @@
-import streamlit as st
-
-import numpy as np
-import pandas as pd
-
-
-st.write("""
-# Simple Stock Price App
-
-Shown are the stock closing price and volume of Microsoft!
-
-""")
-
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+import yfinance as yf
 
 #define the ticker symbol
 tickerSymbol = 'MSFT'
@@ -20,12 +7,7 @@ tickerSymbol = 'MSFT'
 tickerData = yf.Ticker(tickerSymbol)
 
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-1-31')
+tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-1-25')
 
 #see your data
 tickerDf
-
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
-
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
