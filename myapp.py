ff706070cf2717f252b4acd9824171e8ee35fa85
laziaf56 <87532054+laziaf56@users.noreply.github.com>
2021-07-27 11:52:26 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -11,14 +11,20 @@ Shown are the stock closing price and volume of Test!
 
 """)
 
-
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+
 #define the ticker symbol
-tickerSymbol = 'GOOGL'
+tickerSymbol = 'MSFT'
+
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
+
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2020-1-31', end='2020-5-31')
+tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-1-31')
+
+#see your data
+tickerDf
+
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.line_chart(tickerDf.Close)
