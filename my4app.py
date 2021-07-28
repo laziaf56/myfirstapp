0f1d45a819ca4436f02e9b5b2a0aebe8784e8602
laziaf56 <87532054+laziaf56@users.noreply.github.com>
2021-07-28 14:20:16 +0800

    Update my4app.py

--- my4app.py
+++ my4app.py
@@ -4,17 +4,17 @@ import pandas as pd
 
 st.write("""
 # Simple Stock Price App
-Shown are the stock closing price and volume of Google!
+Shown are the stock closing price and volume!
 """)
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['Google','MSFT','AAPL'])
 
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+if option=='Google':
+    tickerSymbol = 'GOOGL'
+    tickerData = yf.Ticker(tickerSymbol)
+    tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+
+    st.line_chart(tickerDf.Close)
+    st.line_chart(tickerDf.Volume)
